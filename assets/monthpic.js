9e32600f95f53c9d1a1fbd26ea6e9f9bbac1d8ae
wernerjoss <werner@hoernerfranzracing.de>
2020-08-25 12:29:52 +0200

    Revert "fix issue #20 - missing Month Pic"
    This reverts commit 0be154190107c5a3d2308dc0916d6b7b8ffbb06c.

--- assets/monthpic.js
+++ assets/monthpic.js
@@ -1,35 +1,23 @@
 /*	new approach for update month picture upon next - prev - home click
-	evaluate Text in Month div fc-left: this is the Month name in current locale
+	evaluate Text in Month div fc-center: this is the Month name in current locale
 	build pic name from that.
 	pic files must be named like months in current locale !
 */
 
 $(document).ready(function() {
-    function fileExists(url) {
-        if(url){
-            var req = new XMLHttpRequest();
-            req.open('GET', url, false);
-            req.send();
-            return req.status==200;
-        } else {
-            return false;
-        }
-    }
-    setTimeout(function() { // ohne Timeout wird activemonth nicht belegt !
-        var debug = false;
-
-        //	var activemonth = $('div.fc-left h2').text();	// fc 3.x
-		var activemonth = $('div.fc-center h2').text();	// fc 4.3
+    var debug = false;
+    var lastmonthname = '';
+    setInterval(function() {    // statt setTimeout() - siehe https://www.w3schools.com/jsref/met_win_settimeout.asp
+    	var activemonth = $('div.fc-center h2').text();	// fc 4.3
         if (debug) {
             console.log('activemonth:', activemonth);
         }
         var monthname = activemonth.split(" ")[0];
-
         if (monthname.length > 0)  {
             var path = document.location.pathname;
             var monthpic = path + '/' + monthname + '.jpg';
-            if (debug) console.log('monthpic:', monthpic);
-            if (fileExists(monthpic))   {   // 25.08.20 see https://github.com/wernerjoss/grav-plugin-fullcalendar/issues/20
+            if (monthname != lastmonthname) {
+                if (debug) console.log('monthpic:', monthpic);
                 $.ajax({
                     type: "HEAD",
                     async: true,
@@ -40,24 +28,28 @@ $(document).ready(function() {
                 }).fail(function() {
                     if (debug) console.log(window.location + '/' + monthname + '.jpg' + ' not found');
                 })
+                lastmonthname = monthname;
                 $(".fc-button").click(function() { // fc-button ist für vor UND zurück !
                     //	activemonth = $('div.fc-left h2').text();	// fc 3.x
                     activemonth = $('div.fc-center h2').text();	// fc 4.3
-                    if (debug) console.log('button clicked, month:', activemonth);
+                    if (debug) console.log('button clicked, month:', activemonth, 'lastmonth:', lastmonthname);
                     monthname = activemonth.split(" ")[0];
-                    $.ajax({
-                        type: "HEAD",
-                        async: true,
-                        url: window.location + '/' + monthname + '.jpg'
-                    }).done(function() {
-                        if (debug) console.log("new monthpic found");
-                        monthpic = path + '/' + monthname + '.jpg';
-                        if (document.getElementById('actMonth') != null) document.getElementById('actMonth').innerHTML = "<img src=" + monthpic + ">";
-                    }).fail(function() {
-                        if (debug) console.log(window.location + '/' + monthname + '.jpg' + ' not found');
-                    })
+                    if (lastmonthname != monthname) {
+                        lastmonthname = monthname;
+                        $.ajax({
+                            type: "HEAD",
+                            async: true,
+                            url: window.location + '/' + monthname + '.jpg'
+                        }).done(function() {
+                            if (debug) console.log("new monthpic found");
+                            monthpic = path + '/' + monthname + '.jpg';
+                            if (document.getElementById('actMonth') != null) document.getElementById('actMonth').innerHTML = "<img src=" + monthpic + ">";
+                        }).fail(function() {
+                            if (debug) console.log(window.location + '/' + monthname + '.jpg' + ' not found');
+                        })
+                    }
                 });
             }
         }
-    }, 10);
+    }, 250);
 })
