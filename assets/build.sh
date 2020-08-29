ac8ae1f087e91365ecf08c0dfad0c6ea60e80fdd
sherpadawan <gstorchi@gmail.com>
2020-08-29 15:46:36 +0000

    sass attempt

--- assets/build.sh
+++ assets/build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-npx webpack --display-modules
+echo "[INFO] Building CSS"
+dir=$(dirname $-1)
+cd $dir
+scss  --watch sass/:css/ &
+
+echo "[INFO] Building JS"
+npx webpack --display-modules &
+
--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 
-
 module.exports = {
   watch: true,
   mode: 'development',
@@ -11,44 +10,7 @@ module.exports = {
   },
   target: 'web',
   module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [
-          'style-loader', 
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          'style-loader', 
-          // Translates CSS into CommonJS
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
-          // Compiles Sass to CSS
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true,
-              sassOptions: {
-                outputStyle: 'compressed',
-              },
-            },
-          },
-        ],
-      },
-    ],
+    /*sass?*/
   },
   resolve: {
     extensions: ['.js'],
