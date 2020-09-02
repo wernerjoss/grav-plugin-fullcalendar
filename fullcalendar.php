d88db7a526677eee4a59c49e3119b609289fec11
sherpadawan <gstorchi@gmail.com>
2020-09-02 09:54:50 +0000

    performance #1

--- fullcalendar.php
+++ fullcalendar.php
@@ -27,29 +27,7 @@ class FullcalendarPlugin extends Plugin
             'onGetPageTemplates'   => ['onGetPageTemplates', 0],
             'onPagesInitialized' => ['onPagesInitialized', 0]
         ]);
-        //add assets
-        $assets = $this->grav['assets'];
-        $assets->addJs('plugin://fullcalendar/assets/dist/bundle.js'); 
-        //@todo use webpack loaders and sass
-        #$assets->addCss('plugin://fullcalendar/assets/css/daygrid.css');  // default CSS for #calendar
-        #$assets->addCss('plugin://fullcalendar/assets/css/tooltip.css');  // default CSS for #calendar
-        $assets->addCss('plugin://fullcalendar/assets/css/main.css');  // default CSS for #calendar
-
-        //map plugin config
-        $config = clone $this->grav['config'];
-        //we choose settings for security reasons
-        $json = "var GRAV = {
-                              'config': { 
-                                'system': { 
-                                   'debugger': ". json_encode($config['system']['debugger'])."
-                                },
-                                'plugins': {
-                                  'fullcalendar': ".json_encode($config['plugins']['fullcalendar']) ."
-                                }
-                              } 
-                            };";
-        $assets->addInlineJs($json, ['loading'=>'inline', 'position'=>'before']); 
-    }
+            }
 
     /**
      * Retrieves ics files which were uploaded on current page (or subpage)
@@ -57,26 +35,49 @@ class FullcalendarPlugin extends Plugin
      */
     public function onPagesInitialized() 
     {
+      //add assets
       $assets = $this->grav['assets'];
-      $taxonomy = $this->grav['taxonomy'];
+      $json = 'var GRAV = GRAV || {};';
+      $json .= " GRAV.page = {header:'', media:''};";
+
       //@todo retrieve files on current page not only subpages
       $pages = $this->grav['page']->evaluate(['@taxonomy.category'=>'calendar']);
       foreach($pages as $page) {
+        $taxonomy = $page->taxonomy();
+        if (!in_array('calendar', $taxonomy['category'])) {
+          throw new Exception('TOTO');
+          return;
+        }
         $headers = json_encode($page->header());
         $media = $page->getMedia();
         $fileUrls = [];
         foreach($media->files() as $name=>$file) {
-           if (substr(strrchr($name, "."), 1) == 'ics') {
+          if (substr(strrchr($name, "."), 1) == 'ics') {
             $fileUrls[] = ['ics'=>$file->url(), 'name'=>$name];
-           }
+          }
         }
         //build page headers and media in json  
-        $assets->addInlineJs(
-          " GRAV.page = {header:'', media:''};" . 
+        $json .= 
           " GRAV.page.header = JSON.parse('" . addslashes($headers) . "');" .
-          (!empty($fileUrls)?" GRAV.page.media = JSON.parse('" . addslashes(json_encode($fileUrls)). "');":''),
-          ['loading'=>'inline', 'position'=>'before']);
+          (!empty($fileUrls)?" GRAV.page.media = JSON.parse('" . addslashes(json_encode($fileUrls)). "');":'');
       }
+
+      $assets->addInlineJs($json, ['loading'=>'inline', 'position'=>'before']);
+      $assets->addJs('plugin://fullcalendar/assets/dist/bundle.js'); 
+      $assets->addCss('plugin://fullcalendar/assets/css/main.css');
+
+      //map plugin config
+      $config = clone $this->grav['config'];
+      //we choose settings for security reasons
+      $json = "GRAV.config = {
+                  'system': { 
+                    'debugger': ". json_encode($config['system']['debugger'])."
+                  },
+                  'plugins': {
+                    'fullcalendar': ".json_encode($config['plugins']['fullcalendar']) ."
+                  }
+              };";
+        $assets->addInlineJs($json, ['loading'=>'inline', 'position'=>'before']); 
     }
 
     public function onTwigTemplatePaths()
